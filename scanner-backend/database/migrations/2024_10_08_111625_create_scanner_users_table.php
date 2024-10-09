6852f47006921868fbfbb5dcf511c7a8ade206d4
laura.borowski <laura.borowski@innogames.com>
2024-10-09 11:11:25 +0200

    migrateee

--- scanner-backend/database/migrations/2024_10_08_111625_create_scanner_users_table.php
+++ scanner-backend/database/migrations/2024_10_08_111625_create_scanner_users_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -15,6 +16,24 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained('user');
             $table->integer('scanner_id');
         });
+        DB::table('user')->insert([
+            [
+                'id' => 1,
+                'username' => 'user',
+                'password' => 'pwd',
+            ]]);
+
+        DB::table('grocery_list')->insert([
+            [
+                'id' => 1,
+                'user_id' => 1
+            ]]);
+
+        DB::table('scanner_user')->insert([
+            [
+                'user_id' => 1,
+                'scanner_id' => 1,
+            ]]);
     }
 
     /**
