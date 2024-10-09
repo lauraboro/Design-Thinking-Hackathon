73c76dee8e64fa5490d4b0b5caf6322052cfe594
laura.borowski <laura.borowski@innogames.com>
2024-10-09 19:57:03 +0200

    increase quantity

--- scanner-backend/app/Http/Controllers/ScanController.php
+++ scanner-backend/app/Http/Controllers/ScanController.php
@@ -18,14 +18,23 @@ class ScanController
         if(!$scannedProduct->successful()) {
             return response()->json(['message' => 'Product not found'], 400);
         }
-        //add the scanned Product to the grocery list
-        $newProduct = new GroceryListProducts();
-        $newProduct->product_barcode = $scannedProduct['code'];
-        $newProduct->product_name = $scannedProduct['product']['product_name'];
 
         $scanUserId = ScannerUser::where('scanner_id', $scannerId)->pluck('user_id')->first();
         $listToAdd = GroceryList::where('user_id', $scanUserId)->pluck('id')->first();
+        $existingProduct = GroceryListProducts::where('grocery_list_id', $listToAdd)
+            ->where('product_barcode', $barcode)
+            ->first();
+
+        if($existingProduct != null) {
+            $existingProduct->product_quantity += 1;
+            $existingProduct->save();
+            return response()->json(['message' => 'Quantity increased'], 201);
+        }
+        //add the scanned Product to the grocery list
+        $newProduct = new GroceryListProducts();
         $newProduct->grocery_list_id = $listToAdd;
+        $newProduct->product_barcode = $scannedProduct['code'];
+        $newProduct->product_name = $scannedProduct['product']['product_name'];
         $newProduct->save();
 
         return response()->json(['message' => 'Product added to the list!'], 201);
